d6e6a914f20ad5bfc6afba2f910adbabc2031f94
Michael Greatorex <magreatorex@gmail.com>
2026-08-08 21:12:01 +0100

    created tf VPC + frontend and backend ECR repositories

---
+++ infra/ecr.tf
@@ -0,0 +1,25 @@
+resource "aws_ecr_repository" "backend" {
+  name                 = "${local.name_prefix}-backend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${local.name_prefix}-backend"
+  }
+}
+
+resource "aws_ecr_repository" "frontend" {
+  name                 = "${local.name_prefix}-frontend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${local.name_prefix}-frontend"
+  }
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -12,3 +12,23 @@ output "name_prefix" {
   description = "Common resource name prefix."
   value       = local.name_prefix
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "backend_ecr_repository_url" {
+  value = aws_ecr_repository.backend.repository_url
+}
+
+output "frontend_ecr_repository_url" {
+  value = aws_ecr_repository.frontend.repository_url
+}
